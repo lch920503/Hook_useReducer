64f6c27acfe9684e63a7bc1b6189a57b7ea93868
lch09253 <lch09253@gmail.com>
2023-10-30 23:56:33 +0900

    feat: useReducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer, useState } from "react";
+
+/*
+  은행
+  예금/출금
+  초기금액 10,000
+  예금 1천원 단위
+  출금 1천원 단위
+*/
+
+const ACTION_TYPE = {
+  deposit: "deposit",
+  withdraw: "withdraw",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPE.deposit:
+      return state + action.payload;
+    case ACTION_TYPE.withdraw:
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [money, dispatch] = useReducer(reducer, 10000);
+  const [number, setNumber] = useState(0);
+
+  const handleDeposit = () => {
+    dispatch({ type: ACTION_TYPE.deposit, payload: number });
+  };
+  const handleWithdraw = () => {
+    dispatch({ type: ACTION_TYPE.withdraw, payload: number });
+  };
+  const changeNumber = (e) => {
+    setNumber(parseInt(e.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>useReducer</h1>
+      <input
+        type="number"
+        value={number}
+        step={1000}
+        onChange={changeNumber}
+        min={0}
+      />
+      <button onClick={handleDeposit}>입금</button>
+      <button onClick={handleWithdraw}>출금</button>
+      <p>{money}</p>
+    </>
   );
 }
 
